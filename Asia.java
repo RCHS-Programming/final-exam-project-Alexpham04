a1f46153aa857f8fdb62602407c3446cb7e0169b
Alexpham04 <Alexander.pham@stu.boone.kyschools.us>
2021-05-13 08:40:09 -0400

    added code to asia

---
+++ Asia.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Asia here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Asia extends World
+{
+
+    /**
+     * Constructor for objects of class Asia.
+     * 
+     */
+    public Asia()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        
+        showText(" match the countries flag to a fact ", 300, 50);
+    }
+}
