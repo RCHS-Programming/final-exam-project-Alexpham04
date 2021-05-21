00a2709483012d5b1279e06cc38e640543c24a3b
Alexpham04 <Alexander.pham@stu.boone.kyschools.us>
2021-05-21 10:00:40 -0400

    added flags

--- Asia.java
+++ Asia.java
@@ -8,8 +8,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Asia extends World
 {
-    String[] flags={ "china flag.png"};
-    String[] questions={" the great wall of china is over 13,000 miles long"};
+    String[] flags={ "china flag.png","vietnamese flag.png","mongolian flag.png"};
+    String[] questions={" has a wall that is over 13,000 miles long"," has the largest desert in asia"};
     int ans= 0;
     /**
      * Constructor for objects of class Asia.
@@ -20,9 +20,11 @@ public class Asia extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
         
-        showText(" match the countries flag to a fact ", 300, 50);
+        showText(" match the countries flag to a question ", 300, 50);
         showText(questions[ans] ,300, 100);
-        addObject(new flag1(), 100, 200);
+        addObject(new flag1(flags[0]), 142, 217);
+        addObject(new flag1(flags[1]), 100, 200);
+        addObject(new flag1(flags[2]), 100, 200);
 
     }
        
--- flag1.java
+++ flag1.java
@@ -8,6 +8,15 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class flag1 extends Actor
 {
+    private String name;
+    /**
+     * Initialize the speed for thr car.
+     */
+    public flag1( String setname)
+    {
+        name = setname;
+        setImage(name);
+    }
     /**
      * Act - do whatever the flag1 wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
